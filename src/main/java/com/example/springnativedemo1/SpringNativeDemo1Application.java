095b93a661e5155181007325a677b11cee61408e
sal <m6567fc@outlook.com>
2021-09-02 18:14:51 +0800

    init

---
+++ src/main/java/com/example/springnativedemo1/SpringNativeDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springnativedemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeDemo1Application.class, args);
+    }
+
+}
